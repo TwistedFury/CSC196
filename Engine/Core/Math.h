9353539fcae48b5593a6d1c441e00daf60cc57b3
Cody Owens <cowens1435@gmail.com>
2025-07-01 10:15:58 -0600

    further setup (7/01)

---
+++ Engine/Core/Math.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace swaws {
+	constexpr float pi = 3.14159265f;
+	constexpr float twoPi = 2 * pi;
+	constexpr float halfPi = pi / 2;
+
+}
--- Source/Game/Main.cpp
+++ Source/Game/Main.cpp
@@ -1,5 +1,11 @@
+#include "Core/Math.h"
 #include <iostream>
+
+#define NAME "Cody\n"
+
 int main()
 {
+	std::cout << NAME;
 	std::cout << "Hello, World!\n";
+	std::cout << swaws::pi << std::endl;
 }
