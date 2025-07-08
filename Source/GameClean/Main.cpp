dfab9ab14c9b2b75ff009b941b9eb0fdfcdcbc38
Cody Owens <cowens1435@gmail.com>
2025-07-08 17:58:35 -0600

    renderer assignment from monday

--- Source/GameClean/Main.cpp
+++ Source/GameClean/Main.cpp
@@ -4,7 +4,10 @@
 int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window* window = SDL_CreateWindow("SDL3 Project", 1280, 1024, 0);
+	int windowWidth = 1280;
+	int windowHeight = 1024;
+
+    SDL_Window* window = SDL_CreateWindow("SDL3 Project", windowWidth, windowHeight, 0);
     if (window == nullptr) {
         std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
         SDL_Quit();
@@ -22,9 +25,6 @@ int main(int argc, char* argv[]) {
     SDL_Event e;
     bool quit = false;
 
-    // Define a rectangle
-    SDL_FRect greenSquare{ 270, 190, 200, 200 };
-
     while (!quit) {
         while (SDL_PollEvent(&e)) {
             if (e.type == SDL_EVENT_QUIT) {
@@ -32,11 +32,43 @@ int main(int argc, char* argv[]) {
             }
         }
 
+
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Set render draw color to black
         SDL_RenderClear(renderer); // Clear the renderer
 
-        SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255); // Set render draw color to green
-        SDL_RenderFillRect(renderer, &greenSquare); // Render the rectangle
+        // Drawing lines
+        for (int i = 0; i < 100; i++) {
+
+            int rand1 = std::rand() % 256;
+            int rand2 = std::rand() % 256;
+            int rand3 = std::rand() % 256;
+            int rand4 = std::rand() % 256;
+
+
+            SDL_SetRenderDrawColor(renderer, rand1, rand2, rand3, rand4); // Set render draw color to random color
+
+            int rand5 = std::rand();
+            int rand6 = std::rand();
+            int rand7 = std::rand();
+            int rand8 = std::rand();
+            SDL_RenderLine(renderer, rand5, rand6, rand7 % windowWidth, rand8 % windowHeight);
+        }
+
+        // Drawing points
+        for (int i = 0; i < 200; i++) {
+
+            int rand1 = std::rand() % 256;
+            int rand2 = std::rand() % 256;
+            int rand3 = std::rand() % 256;
+            int rand4 = std::rand() % 256;
+
+
+            SDL_SetRenderDrawColor(renderer, rand1, rand2, rand3, rand4); // Set render draw color to random color
+
+            int rand5 = std::rand();
+            int rand6 = std::rand();
+            SDL_RenderPoint(renderer, rand5 % windowWidth, rand6 % windowHeight);
+        }
 
         SDL_RenderPresent(renderer); // Render the screen
     }
